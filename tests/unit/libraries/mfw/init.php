9c6855ac513b617afe5985add18564fe474e787e
Lahmizzar Muinela <lahmizzar@devxive.com>
2013-10-24 17:23:47 +0200

    setup basic test env

---
+++ tests/unit/libraries/mfw/init.php
@@ -0,0 +1,24 @@
+<?php
+/**
+  * @info          $Id$ - $Revision$
+  * @package       $mootombo$
+  * @subpackage    RDK
+  * @author        $Author$ @ devXive - research and development <support@devxive.com>
+  * @copyright     Copyright (C) 1997 - 2013 devXive - research and development (http://www.devxive.com)
+  * @license       http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+  * @assetsLicense devXive Proprietary Use License (http://www.devxive.com/license)
+  */
+
+// no direct access
+defined('_MFWRA') or die;
+
+/**
+ * MOOTOMBO!Framework Init class
+ *
+ * @package  MOOTOMBO!Framework
+ * @since    1.0
+ */
+abstract class MFWInit
+{
+
+}
